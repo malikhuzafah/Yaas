e2ff9e662a4610b74da0d02a64d730d148d0f249
malikhuzafah <huzafahrajput10@gmail.com>
2021-02-07 16:19:21 +0500

    YAAS

--- site/js/script.js
+++ site/js/script.js
@@ -1,6 +1,14 @@
 
 window.onscroll = function() {scrollFunction()};
 $(window).on("scroll", function() {
+      if (screen.width <= 992) {
+            var x = document.getElementsByClassName('nav-link-span');
+            for (var i=0;i<x.length;i++) {
+                  x[i].style.color = "#FFDB58";
+            }
+            document.getElementById('navbarDropdown').style.color = "#FFDB58";
+            return;
+      }
       if($(window).scrollTop() > 100) {
             var x = document.getElementsByClassName('nav-link-span');
             for (var i=0;i<x.length;i++) {
@@ -35,3 +43,9 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function todayDate(){
+        var d = new Date();
+        var n = d.getFullYear() + "  ";
+        return document.getElementById("date").innerHTML = n;
+      }
